8fbe6bc785b76a1d81f89c283b9e5b3130ba10d3
Daniel Obi <obidaniel7717@gmail.com>
2026-04-22 13:09:05 +0100

    auto-sync: 1:09:04 PM

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { JetBrains_Mono } from "next/font/google";
 import "./globals.css";
-import Preloader from "@/components/preloader/Preloader";
 
 const jetbrainsMono = JetBrains_Mono({
   variable: "--font-jetbrains-mono",
