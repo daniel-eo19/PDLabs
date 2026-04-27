8134287c7e9e6a99ca3d74f298a45d64c132b5ef
Daniel Obi <obidaniel7717@gmail.com>
2026-04-27 17:42:32 +0100

    auto-sync: 5:42:32 PM

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { ArrowLeft, ArrowUpRight } from "lucide-react";
+import ThemeToggle from "@/components/ThemeToggle";
 import { ThemeProvider } from "@/providers/themeProvider/ThemeProvider";
 import Button from "@/components/button/Button";
 import SceneDecorations from "@/components/background/SceneDecorations";
